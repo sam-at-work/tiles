b1669d92ccf055d807589a1c3fde2e4a8ed888b0
Sam Grace <sam@festicket.com>
2018-03-30 13:13:58 +0100

    wip - using clip path, adding logic for pipe types

--- src/components/Tile/index.jsx
+++ src/components/Tile/index.jsx
@@ -11,19 +11,118 @@ const Tile = styled.div`
   display: inline-block;
   width: ${tileWidth};
   height: ${tileHeight};
-  background-color: red;
   margin: 5px;
   transform: rotate(${props => props.rotation}turn);
-  transition: transform 1s;
+  transition: transform 1s; /* dump */
+  overflow: hidden;
+  will-change: transform;
 `;
+
+// const Pipe = styled.div`
+//   position: absolute;
+//   height: 50%;
+//   width: 50%;
+//   background-color: aqua;
+//   &:before {
+//     content: "";
+//     position: absolute;
+//     height: 100%;
+//     width: 100%;
+//     border: 1px solid black;
+//     border-radius: 50%;
+//     left: -50%;
+//     top: -50%;
+//   }
+//   &:after {
+//     content: "";
+//     position: absolute;
+//     height: 110%;
+//     width: 110%;
+//     border: 1px solid black;
+//     border-radius: 50%;
+//     left: -55%;
+//     top: -55%;
+//   }
+// `;
+
+const Pattern = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: sandybrown;
+
+  .water {
+    position: absolute;
+    background-color: lightblue;
+    height: 105%;
+    width: 105%;
+    top: -50%;
+    left: -50%;
+    clip-path: circle(50%);
+  }
+
+  .inner-pattern {
+    position: absolute;
+    background-color: sandybrown;
+    height: 95%;
+    width: 95%;
+    top: -50%;
+    left: -50%;
+    clip-path: circle(50%);
+  }
+
+  .cross-hairs {
+    &:before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 50%;
+      width: 1px;
+      height: 100%;
+      background-color: black;
+    }
+    &:after {
+      content: "";
+      position: absolute;
+      top: 50%;
+      left: 0;
+      width: 100%;
+      height: 1px;
+      background-color: black;
+    }
+  }
+  //.outer-circle {
+  //  position: absolute;
+  //  background-color: pink;
+  //  height: 100%;
+  //  width: 100%;
+  //  border-radius: 50%;
+  //  top: -50%;
+  //  left: -50%;
+  //}
+`;
+
+const Pipe = () => (
+  <Pattern>
+    <div className={"water"}>dsa</div>
+    <div className={"inner-pattern"}>dsa</div>
+    <div className={"cross-hairs"} />
+  </Pattern>
+);
 
 export default class FunctionalTile extends React.Component<
   {},
   { rotation: number }
 > {
   state = {
-    rotation: 0
+    rotation: Math.floor(Math.random() * tileSides)
   };
+
+  type: number; // number of edges between entering and exiting of pipe.
+
+  constructor() {
+    super(constructor);
+    this.type = Math.floor(Math.random() * tileSides / 2);
+  }
 
   onClick = (event: SyntheticEvent<HTMLDivElement> & { pageX: number }) => {
     const middleX = event.currentTarget.offsetWidth / 2;
@@ -39,6 +138,10 @@ export default class FunctionalTile extends React.Component<
 
   render() {
     const { rotation } = this.state;
-    return <Tile onClick={this.onClick} rotation={rotation / tileSides} />;
+    return (
+      <Tile onClick={this.onClick} rotation={rotation / tileSides}>
+        <Pipe type={this.type} />
+      </Tile>
+    );
   }
 }
