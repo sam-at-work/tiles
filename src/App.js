cc23693b77364385ea16e7b9d077323ab0471b8c
Sam Grace <sam@festicket.com>
2018-04-07 20:33:26 +0100

    App to SFC

--- src/App.js
+++ src/App.js
@@ -4,27 +4,22 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 import { startGame } from "./actionCreators";
-
 import Board from "./components/Board";
 
-class App extends Component<{}> {
-  render() {
-    if (this.props.game.gameStarted) {
-      return <Board />;
-    } else {
-      return (
-        <div>
-          Ready to play?
-          <button onClick={() => this.props.dispatch(startGame())}>Yes!</button>
-        </div>
-      );
-    }
+function App({ game, dispatch }) {
+  if (game.gameStarted) {
+    return <Board />;
   }
+  return (
+    <div>
+      Ready to play?
+      <button onClick={() => dispatch(startGame())}>Yes!</button>
+    </div>
+  );
 }
 
 const mapStateToProps = state => ({
   game: state.game,
-  board: state.board,
 });
 
 export default connect(mapStateToProps)(App);
