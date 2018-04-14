de93ea15253e0ba954310a5e9cf17228963abb34
Sam Grace <sam@festicket.com>
2018-04-14 15:34:20 +0100

    Change level generator

--- src/functions/level-generator.js
+++ src/functions/level-generator.js
@@ -15,8 +15,8 @@ export function levelGenerator(): boardMeta {
 
 export function boardSizeGenerator() {
   const { level, gameType } = store.getState().game;
-  const LEVELS_PER_STAGE = 3;
   const startingWidth = 5;
+  const LEVELS_PER_STAGE = startingWidth;
   const width = startingWidth + Math.floor((level - 1) / LEVELS_PER_STAGE);
   const height = width + (level - 1) % LEVELS_PER_STAGE;
 
